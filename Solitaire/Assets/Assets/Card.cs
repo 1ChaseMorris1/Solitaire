d9ec4f136ae53e16f27cf4cb539d501bf719ecda
Chase Morris <plants7051@gmail.com>
2022-10-02 20:41:27 -0400

    10/2/2022

--- Solitaire/Assets/Assets/Card.cs
+++ Solitaire/Assets/Assets/Card.cs
@@ -6,21 +6,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHandler, IBeginDragHandler
+public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHandler, IBeginDragHandler, IPointerUpHandler
 {
-    private Engine engine;  // game engine
-    private int type;           // spades, diamonds etc 
-    private char color;         // black or red 
-    public int Class;          // ace, two, king etc 
-    private Sprite back;        // is used incase card is turned over
-    private Sprite card;        // the front sprite of the card
-    private int position;           // cards postition in the deck of cards
-    private bool flipped;           // is card facing front
-    public bool inStack;           // if the card is in the stack then this will be true.
-    public bool last_card;            // if the card is the last in the stack then a card can be placed onto it
-    private GameObject nextCard;       // the next card in the line
+    private Engine engine;                // game engine
+    private int type;                     // spades, diamonds etc 
+    private char color;                   // black or red 
+    public int Class;                     // ace, two, king etc 
+    private Sprite back;                  // is used incase card is turned over
+    private Sprite card;                  // the front sprite of the card
+    private bool flipped;                 // is card facing front
+    public bool inStack;                  // if the card is in the stack then this will be true.
+    public bool last_card;                // if the card is the last in the stack then a card can be placed onto it
+    private Vector2 previousPosition;     // the previous position of the card. 
+    public int stack;                     // what stack this card is in. 
 
-    public void create_card(int Class, int type, Sprite card, Sprite back, int p)
+    // ** MAYBE ** 
+    private Card nextCard;                // the next card in the line.
+    private Card previousCard;            // the previous card in the line. 
+
+    public void create_card(int Class, int type, Sprite card, Sprite back)
     {
         nextCard = null;
         last_card = false;
@@ -29,7 +33,6 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
         this.type = type;
         this.back = back;
         this.card = card;
-        position = p;
         flipped = false;
         inStack = false;
 
@@ -42,6 +45,7 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
 
     }
 
+    /*
     public void equals(Card card)
     {
         engine = card.engine;
@@ -49,7 +53,6 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
         type = card.type;
         back = card.back;
         this.card = card.card;
-        position = card.position;
 
         if (type > 2)
             color = 'B';
@@ -58,6 +61,7 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
 
         gameObject.GetComponent<Image>().sprite = card.back;
     }
+    */
 
     public void flipCardBack()
     {
@@ -71,21 +75,6 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
         flipped = true;
     }
 
-    // moves the card down and flips it over. 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        // if flipped 
-        if (!flipped && !inStack)
-        {
-            var y = gameObject.GetComponent<RectTransform>().anchoredPosition.y;
-            y = y - 239;
-            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-849f, y);
-            flipCardFront();
-            transform.SetSiblingIndex(55);
-            engine.move_deck();
-        }
-    }
-
     // moves the card up and flips it over when the deck has been fully shuffled.
     public void moveCardUp()
     {
@@ -95,87 +84,80 @@ public class Card : MonoBehaviour, IPointerClickHandler, IDragHandler, IDropHand
         flipCardBack();
     }
 
+    public void revertPosition()
+    {
+        GetComponent<RectTransform>().anchoredPosition = previousPosition;
+    }
+
+    /***********************************************************************
+     * MOVEMENT FUNCTIONS                                                  *
+     ***********************************************************************/ 
+
     public void OnDrag(PointerEventData eventData)
     {
         if (flipped)
         {
 
             gameObject.GetComponent<RectTransform>().anchoredPosition +=
-                eventData.delta / Engine.canvas.GetComponent<Canvas>().scaleFactor;
+                eventData.delta / engine.canvas.GetComponent<Canvas>().scaleFactor;
 
             transform.SetSiblingIndex(60);
-            // fix
-            moveAllCards(eventData.pointerDrag.gameObject, 50);
-            
-            //transform.SetSiblingIndex(81);
-
-            /*
-            for(int i = 0; i < engine.eventDrop.Count; i++)
-            {
-                engine.eventDrop[i].transform.SetSiblingIndex(transform.GetSiblingIndex() + 1);
-            }
-            */
         }
 
-    }
-
-    // when the appropriate card is dropped onto this card, the card will be set to 
-    // the position of this card minus 50 
-
-    // finds the last card in the stack and sets its end variable to true.
-    private void addCard(GameObject card, Card next)
-    {
-        if (card == null)
-        {
-            card.AddComponent<Card>();
-            card.GetComponent<Card>().equals(next); 
-            card.GetComponent<Image>().sprite = null;
-            return;
-
-        } else
-        {
-           addCard(card.GetComponent<Card>().nextCard, next);
-        }
-    }
-
-    private void moveAllCards(Card card, int tick)
-    {
-        if (card == null)
-        {
-            print("false");
-            return;
-        } else
-        {
-            print("made");
-
-            card.transform.SetSiblingIndex(60);
-
-            var y = GetComponent<RectTransform>().anchoredPosition.y;
-            var x = GetComponent<RectTransform>().anchoredPosition.x;
-
-            card.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y - tick);
-
-            moveAllCards(card.GetComponent<Card>(), tick + 50);
-        }
     }
 
     public void OnDrop(PointerEventData eventData)
     {
         eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        var x = GetComponent<RectTransform>().anchoredPosition.x;
-        var y = GetComponent<RectTransform>().anchoredPosition.y;
+        if (last_card)
+        {
+            eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y - 50);
+            var x = GetComponent<RectTransform>().anchoredPosition.x;
+            var y = GetComponent<RectTransform>().anchoredPosition.y;
 
-        print(type);
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y - 50);
 
-        addCard(this, eventData.pointerDrag.gameObject.GetComponent<Card>());
+        }
+        else
+        {
+            eventData.pointerDrag.GetComponent<Card>().revertPosition();
+        }
 
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        previousPosition = GetComponent<RectTransform>().anchoredPosition;
         GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+
+    // moves the card down and flips it over. 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // if flipped 
+        
+        if (!flipped && !inStack)
+        {
+            var y = gameObject.GetComponent<RectTransform>().anchoredPosition.y;
+            y = y - 239;
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-849f, y);
+            flipCardFront();
+            transform.SetSiblingIndex(60);
+            engine.move_deck();
+        }
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.pointerClick.GetComponent<Card>().flipped)
+        {
+            eventData.pointerDrag.GetComponent<Card>().revertPosition();
+            eventData.pointerDrag.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+
     }
 }
--- Solitaire/Assets/Assets/Engine.cs
+++ Solitaire/Assets/Assets/Engine.cs
@@ -8,33 +8,30 @@ using UnityEngine.UI;
 
 public class Engine : MonoBehaviour
 {
-    // +279.7 initial jump, after that the space is +189.3  
     // ace is a 1 
     // king is highest at 14 
     // if 1 or 4 card is black else card is red
-    // for flipping card move -239 down
 
-    [SerializeField] private Sprite back_card;      // back of card sprite 
+    [SerializeField] private Sprite back_card;                      // back of card sprite.
+    public List<Card> deck = new List<Card>();                      // deck of cards, only one in game.
+    private Stack[] stack;                                          // the empty decks on the board
+    private int que_deck = 52;                                      // number of cards in que.
+    private int deck_iterator = 0;                                  // counts stuff
+    [SerializeField] private List<Sprite> card_fronts;              // front sprite for cards
+    public GameObject canvas;                                       // the games canvas 
 
-    public static List<Card> deck = new List<Card>();                  // deck of cards
-    private static List<List<int>> stacks = new List<List<int>>();      // stakcs of cards.
-
-    private static int que_deck = 52;                                               // number of cards in que.
-    private static int deck_iterator = 0;                                           // counts stuff
-
-    [SerializeField] private List<Sprite> card_fronts;               // front sprite for cards
-
-    public static GameObject canvas;                            // the games canvas 
-    public List<Drop> eventDrop = new List<Drop> ();        
 
     private void Start()
     {
-
         canvas = GameObject.FindGameObjectWithTag("game");
+
+        GameObject stack = GameObject.FindGameObjectWithTag("stack");
+
+        this.stack = stack.GetComponentsInChildren<Stack>();
 
         generateDeck();
 
-        shuffle();
+       // shuffle();
 
         setDecks();
 
@@ -58,7 +55,7 @@ public class Engine : MonoBehaviour
 
                 deck[k].transform.position = card.transform.position;
 
-                deck[k].create_card(i, j, card_fronts[k], back_card,k);
+                deck[k].create_card(i, j, card_fronts[k], back_card);
 
                 k++;
             }
@@ -73,43 +70,30 @@ public class Engine : MonoBehaviour
 
     }
 
-    // sets the decks on the board 
-    /*****************************
-     * 279.7 is the first X jump
-     * after that 189.3 is the rest of the 
-     * jumps, will keep track of this 
-     * with a variable jump. 
-     * static Y value is 269.
-     * static X value is -849
-     *****************************/
+    // sets all the decks up 
     private void setDecks()
     {
-        float jump = -849f;
+
         int k = 1;
         int l = 0;
-        //int z = 0;
         int down = 50;
-        Drop drop = GameObject.FindGameObjectWithTag("eventdrop").GetComponent<Drop>();
+
         // 7 is the number of decks.
         // I is the deck I am acessing
         for (int i = 0; i < 7; i++)
         {
-            stacks.Add(new List<int>());
-
-            if (k == 1)
-                jump = jump + 279.7f;
-            else
-                jump = jump + 189.3f;
 
             // k is the number of cards
             for(int j = 0; j < k; j++)
             {
 
-                deck[l].GetComponent<RectTransform>().anchoredPosition = new Vector2(jump, 269);
+                deck[l].GetComponent<RectTransform>().anchoredPosition = stack[i].GetComponent<RectTransform>().anchoredPosition;
+
+                
 
                 deck[l].inStack = true;
 
-                stacks[i].Add(l);
+                deck[l].stack = j;
 
                 if(j > 0)
                 {
@@ -126,14 +110,6 @@ public class Engine : MonoBehaviour
                     deck[l].flipCardFront();
                     deck[l].last_card = true;
 
-                    /*
-                    eventDrop.Add(Instantiate(drop));
-                    eventDrop[z].transform.SetParent(canvas.transform);
-                    eventDrop[z].transform.SetSiblingIndex(1);
-                    eventDrop[z].GetComponent<RectTransform>().anchoredPosition = deck[l].GetComponent<RectTransform>().anchoredPosition;
-
-                    z++;
-                    */
                 }
 
                 l++;
